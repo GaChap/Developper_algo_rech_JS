008d9d28a25b43644f5647671f9a7f297d5d1cbb
GaChap <66822976+GaChap@users.noreply.github.com>
2023-01-05 15:34:03 +0100

    bug_select
    le sélecteur des ingrédients déconne pour je ne sais quelle raison

--- index.js
+++ index.js
@@ -40,6 +40,83 @@ function generer_tag(type, value) {
     appendElement(tagChosen, [tagChosenText, tagChosenMark]);
     appendElement(querySelector("#tag_chosen"), [tagChosen]);
 }
+//Fonction pour faire le rendu
+const render = (word =' ')=>{
+    //galerie.innerHTML = "";
+    word = word.trim().toLowerCase();
+    const filtered = filterData(word);
+    generer_carte(filtered);
+}
+//Fonction pour filtrer les données suivant un string
+const filterData = (word) =>{
+    return recipes.filter(
+        (item)=>
+        item.name.toLowerCase().includes(word)||
+        item.description.toLowerCase().includes(word)||
+        item.ingredients.map((ingr)=>{ ingr.ingredient.toLowerCase()}).includes(word)
+    );
+}
+//Fonction pour générer les carte de recette en suivant un array
+const generer_carte = (donnees) => {
+    galerie.innerHTML = "";
+    donnees.forEach((recipe)=>{
+        //génération de la carte
+        const recipe_card = createElement('div');
+        classAdd(recipe_card, ['recipe_card']);
+        const recipe_img = createElement('div');
+        classAdd(recipe_img, ['recipe_img']);
+        const recipe_info = createElement('div');
+        classAdd(recipe_info, ['recipe_info']);
+        const recipe_title = createElement('span');
+        classAdd(recipe_title, ['recipe_title']);
+        recipe_title.id = "Test";
+        recipe_title.innerText = recipe.name;
+        const recipe_info_up = createElement('div');
+        classAdd(recipe_info_up, ['recipe_info_up_time']);
+        const time_icon = createElement('i');
+        classAdd(time_icon, ["recipe_time_icon", "fa-regular", "fa-clock"]);
+        const time_quantite = createElement('span');
+        time_quantite.innerText = `${recipe.time} min`;
+        classAdd(time_quantite, ['recipe_time']);
+        appendElement(recipe_info_up, [time_icon, time_quantite]);
+        const recipe_info_down = createElement("div");
+        classAdd(recipe_info_down, ['recipe_info_down_ingr']);
+        recipe.ingredients.forEach((ingr)=>{
+            const leIngredient = createElement('div');
+            classAdd(leIngredient, ['ingredient']);
+            const ingredient = createElement("span");
+            let ingr_unit = ' ';
+            let ingr_quantity = ' ';
+            const ingr_name = createElement('span');
+            ingr_name.innerText = `${ingr.ingredient}: `;
+            ingr_name.style.fontWeight = "bold";
+            ingr_name.style.marginRight = "5px";
+            if(ingr.quantity != undefined && ingr.quantity != null){ingr_quantity = ingr.quantity}
+            if(ingr.unit != undefined && ingr.unit != null){ingr_unit = ingr.unit}
+            switch(ingr_unit){
+                case 'cuillères à soupe': ingr_unit = 'cuill. soupe';
+                break
+                case 'cuillères à café': ingr_unit = 'cuill. café';
+                break
+                case 'grammes': ingr_unit = 'g';
+                break
+            }
+            ingredient.innerText = ` ${ingr_quantity} ${ingr_unit}`;
+            appendElement(leIngredient, [ingr_name, ingredient]);
+            appendElement(recipe_info_down, [leIngredient]);
+        })
+        const recipe_desc = createElement('div');
+        classAdd(recipe_desc, ['recipe_descript']);
+        const recipe_desc_text = createElement("p");
+        recipe_desc_text.innerText = recipe.description;
+        classAdd(recipe_desc_text, ['recipe_descript_text']);
+        appendElement(recipe_desc, [recipe_desc_text]);
+        appendElement(recipe_info, [recipe_title, recipe_info_up, recipe_info_down, recipe_desc]);
+        appendElement(recipe_card, [recipe_img, recipe_info]);
+        appendElement(galerie, [recipe_card]);
+        //filterOptionsAppa()
+    })
+}
 
 //Processus pour supprimer les doublons
 const ingredients = [];
@@ -57,6 +134,9 @@ recipes.forEach((Recipe) => {
 const uniqueIngredients = Array.from(new Set(ingredients));
 const uniqueAppareils = Array.from(new Set(appareils));
 const uniqueUstensiles = Array.from(new Set(ustensiles));
+const galerie = querySelector('#galerie');
+generer_carte(recipes);
+const galerie_content = document.querySelectorAll('.recipe_card');
 let n = 0;
 //Debut génération select
 for (let i = 0; i < 3; i++) {
@@ -81,6 +161,7 @@ for (let i = 0; i < 3; i++) {
                 const option = createElement("p");
                 option.innerText = uniqueIngredients[i];
                 appendElement(options, [option]);
+                //console.log(options.childNodes);
                 option.addEventListener('click', (e) => {
                     if (option.classList.contains("active") == false) {
                         classAdd(option, ["active"]);
@@ -92,7 +173,7 @@ for (let i = 0; i < 3; i++) {
         case 1: select_button.value = "Appareils";
             selecteur.id = "appareils_select";
             classAdd(options, ["appa_options"]);
-            for (let i = 0; i < 6; i++) {
+            for (let i = 0; i < uniqueAppareils.length; i++) {
                 const option = createElement("p");
                 option.innerText = uniqueAppareils[i];
                 appendElement(options, [option]);
@@ -104,7 +185,7 @@ for (let i = 0; i < 3; i++) {
         case 2: select_button.value = "Ustensiles";
             selecteur.id = "ustensiles_select";
             classAdd(options, ["ust_options"]);
-            for (let i = 0; i < 6; i++) {
+            for (let i = 0; i < uniqueUstensiles.length; i++) {
                 const option = createElement("p");
                 option.innerText = uniqueUstensiles[i];
                 appendElement(options, [option]);
@@ -155,7 +236,7 @@ for (let i = 0; i < 3; i++) {
     })
     //Pour "fermer" le sélecteur
     querySelector(".main_container").addEventListener("click", (e) => {
-        if (!e.target.closest(".search_tag") && n != 0) {
+        if (!e.target.closest(".container") && n != 0) {
             if (selecteur.children[1] != selectorIcon) {
                 selecteur.children[1].style.display = "none";
             }
@@ -177,6 +258,8 @@ for (let i = 0; i < 3; i++) {
 querySelector("#search_princip").addEventListener("input", (e) => {
     const valeur = querySelector("#search_princip").value.toLowerCase();
     if (valeur.length >= 3) {
+        render(valeur);
+        
         //Algorithme naïf titre
         const TestRecipName = recipes.filter(function (item2) {
             const nameArray = item2.name.split(' ');
@@ -203,6 +286,9 @@ querySelector("#search_princip").addEventListener("input", (e) => {
             }
         })
         const TestRecipIngr2 = recipes.filter(function (item) {
+            /*const ingrArray = item.ingredients.map(function(ingr){
+                ingr.ingredient.toLowerCase()
+            }).split(' ').includes(valeur);*/
             const ingrArray = item.ingredients;
             const ingrNameArray = ingrArray.map(function (ingr) {
                 return ingr.ingredient.toLowerCase().split(' ');
@@ -232,12 +318,6 @@ querySelector("#search_princip").addEventListener("input", (e) => {
             if (descArray.includes(true)) { return true }
             return descArray.includes(valeur);
         });
-        const re = /\s*[,\.\(\)]\s*/;
-        const TestMdr = "Manger, c'est, délicieux. Je vais me faire (péter le bide)";
-        const testRT = [{ name: "test1" }, { name: "test2" }, { name: "test3" }]
-        const TestLol = ["manger", "test", "boire"];
-        //console.log([TestRecipDesc, TestRecipIngr, TestRecipName]);
-        //console.log(TestMdr.split(re));
         let ObjectTab = [];
         if (TestRecipDesc != undefined || TestRecipIngr != undefined || TestRecipName != undefined) {
             let compte = [TestRecipDesc.length, TestRecipIngr.length, TestRecipName.length];
@@ -257,6 +337,48 @@ querySelector("#search_princip").addEventListener("input", (e) => {
             }
         }
         const ResultRech = Array.from(new Set(ObjectTab));
-        console.log(ResultRech);
+        //console.log(ResultRech);
+        //console.log(filterData('coco'));
+    }else{
+        if(valeur.length < 3){
+            if(galerie_content != undefined && galerie_content != null && galerie_content != document.querySelectorAll('.recipe_card')){
+                //generer_carte(recipes);
+            }
+        }
     }
 })
+function filterOptionsIngre(word){
+    const ArrayTest = Array.from(querySelector('.ingre_options').childNodes);
+    const filtered = ArrayTest.filter(function(item){
+        return item.innerText.split(' ').includes(word);
+    })
+    if(filtered != undefined || filtered != null){
+        querySelector('.ingre_options').innerHTML = '';
+        filtered.forEach((option)=>{
+            const uneOption = createElement("p");
+            uneOption.innerText = option.innerText;
+            appendElement(querySelector('.appa_options'), [uneOption]);
+        })
+    }
+}
+filterOptionsIngre("coco");
+
+function filterOptionsAppa(){
+    //console.log(document.querySelector("#ingredients_select"));
+    //const ArrayTest = Array.from(querySelector('.appa_options').childNodes);
+    
+    const ArrayTest2 = Array.from(document.querySelectorAll(".recipe_card"));
+    const ArrayTest3 = ArrayTest2.map(function (item){
+        return item.lastChild.firstChild.innerText;
+    })
+    const Test4 = recipes.filter(function (recipe){
+        return ArrayTest3.includes(recipe.name)
+    })
+    if(Test4.length != recipes.length){
+        const Test5 = Test4.map(function(recipe){
+            return recipe.appliance;
+        })
+        const Test6 = Array.from(new Set(Test5));
+        console.log(Test6)
+    }
+}
